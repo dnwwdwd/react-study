0096df676e2625754424e16563e93a3ce9e792bc
hejj19 <hejj19@asiainfo.com>
2024-07-14 17:27:58 +0800

    add README.md add Redux、router、自定义 Hook 学习 add useReducer 学习

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, {useEffect, useState} from "react";
+import React, {useMemo, useState} from "react";
 
-
-// 问题：布尔切换逻辑与当前组件耦合在一起，不方便使用
-// 解决思路：自定义 hook
-
-function useToggle() {
-    // 可复用的代码逻辑
-    const [value, setValue] = useState(true);
-
-    const toggle = () => setValue(!value);
-    // 哪些状态和回调函数需要在其他组件使用 return
-    return {
-        value,
-        toggle
+function fib(n) {
+    console.log('计算函数执行了');
+    if (n < 3) {
+        return 1;
     }
+    return fib(n - 2) + fib(n - 1);
 }
 
 function App() {
+    const [count1, setCount1] = useState(0);
 
-    const { value, toggle } = useToggle();
+    const result = useMemo(() => {
+        return fib(count1);
+    }, [count1]);
 
+    // const result = fib(count1);
+    const [count2, setCount2] = useState(0);
+    console.log('组件重新渲染了');
     return (
         <div className="App">
-            {value && <div>this is div</div>}
-            <button onClick={toggle}>toggle</button>
+            this is app
+            <button onClick={() => setCount1(count1 + 1)}>change count1: {count1}</button>
+            <button onClick={() => setCount2(count2 + 1)}>change count2: {count2}</button>
+            result is {result}
         </div>
     );
 }
